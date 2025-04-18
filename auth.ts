96fdcf9dc454bdd0913528e659b7f89e304ebf54
Daniel Lima <130805695+daniwells@users.noreply.github.com>
2025-04-18 12:47:33 -0300

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
-const allowedEmails = ["daniellimafreita@gmail.com"]
+const allowedEmails = ["adm@gmail.com"]
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
