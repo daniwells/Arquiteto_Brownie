bba7a19387ef995fd0d53d853d00ca0bd878e59b
daniel <daniellimafreita@gmail.com>
2025-05-29 20:50:58 -0300

    :sparkles: feat: Desenvolvendo responsividade para a página de login

--- src/app/(auth)/sign-in/sign-in.tsx
+++ src/app/(auth)/sign-in/sign-in.tsx
@@ -1,9 +1,16 @@
 'use client';
 
-import React from 'react';
+// Libs
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+// Style
 import * as S from './styles';
+
+// Components
 import PrimaryButton from '@/interface/components/global/primary-button/main';
 import DescriptionContainer from '@/interface/containers/global/description-container/main';
+
+// Images
 import logo from '../../../../public/svg/logo.svg';
 
 interface signInProps {
@@ -11,17 +18,28 @@ interface signInProps {
 }
 
 const SignIn: React.FC<signInProps> = ({ handleSignIn }) => {
+  const size_768 = useMediaQuery('(min-width:768px)');
+
   return (
     <S.MainLogin>
-      <S.customImage width={70} src={logo} alt="Logo Arquiteto Brownie" />
+      {
+        !size_768 &&
+        <S.customImage width={70} src={logo} alt="Logo Arquiteto Brownie" />
+      }
       <S.ContentLogin>
-        <DescriptionContainer
-          title="Login"
-          desc="Faça login com uma conta google autorizada para acessar a página de administração"
-        />
-        <form action={handleSignIn}>
-          <PrimaryButton category="dark" type="submit" value="Entrar com o Google" />
-        </form>
+        {
+          size_768 &&
+          <S.customImage width={200} src={logo} alt="Logo Arquiteto Brownie" />
+        }
+        <div> 
+          <DescriptionContainer
+            title="Login"
+            desc="Faça login com uma conta google autorizada para acessar a página de administração"
+          />
+          <form action={handleSignIn}>
+            <PrimaryButton category="dark" type="submit" value="Entrar com o Google" />
+          </form>
+        </div>
       </S.ContentLogin>
     </S.MainLogin>
   );
--- src/app/(auth)/sign-in/styles.ts
+++ src/app/(auth)/sign-in/styles.ts
@@ -1,37 +1,61 @@
 import styled from 'styled-components';
 import { colors } from '@/styles/themes';
 import Image from 'next/image';
+import breakpoints from '@/styles/breakpoints';
 
 export const customImage = styled(Image)`
   margin: 20px;
 `;
 
 export const MainLogin = styled.main`
+  width: 100%;
   min-height: 100vh;
   background-color: ${colors.baseYellow};
   background-image: url('/images/background-signin.png');
   background-size: contain;
   background-repeat: no-repeat;
   background-position: center center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  @media ${breakpoints.md} {
+    justify-content: end;
+  }
 `;
 
 export const ContentLogin = styled.div`
   background-color: white;
-  position: fixed;
   bottom: 0;
   padding: 20px;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
-  gap: 20px;
-  height: 70vh;
-  min-height: 18rem;
-  max-height: 26rem;
-
+  height: 75vh;
   width: 100%;
-  > form {
+  >form{
     width: 100%;
   }
   border-radius: 20px 20px 0px 0px;
+
+  >form{
+    display: flex;
+    justify-content: center;
+  }
+
+  >div{
+    max-width: 450px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+
+  @media ${breakpoints.md} {
+    height: 90vh;
+    gap: 100px;
+  }
+
+  @media ${breakpoints.lg} {
+    gap: 200px;
+  }
 `;
