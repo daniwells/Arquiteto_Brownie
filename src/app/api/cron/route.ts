57aa1315e35bd68c68d6fc38f6a059f48ef30ebc
daniel <daniellimafreita@gmail.com>
2025-07-11 03:56:55 -0300

    :white_check_mark: test: Adicionando um minuto de inatividade para a API de anonymous

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,23 +3,24 @@ import { anonymizeCustomer } from '@/lib/services/anonymizeCustomer';
 import { prisma } from '@/db/prisma';
 
 export async function POST() {
-  const daysInactive = 1;
-  const cutoff = new Date();
-  cutoff.setDate(cutoff.getDate() - daysInactive * 60 * 1000);
+    const daysInactive = 1;
+//   const cutoff = new Date();
+//   cutoff.setDate(cutoff.getDate() - daysInactive);
+    const cutoff = new Date(Date.now() - daysInactive * 60 * 1000);
 
-  const customers = await prisma.customer.findMany({
-    where: {
-      isAnonymized: false,
-      updatedAt: { lt: cutoff },
-    },
-  });
+    const customers = await prisma.customer.findMany({
+        where: {
+        isAnonymized: false,
+        updatedAt: { lt: cutoff },
+        },
+    });
 
-  for (const customer of customers) {
-    const data = anonymizeCustomer(customer);
-    await prisma.customer.update({ where: { id: customer.id }, data });
-  }
+    for (const customer of customers) {
+        const data = anonymizeCustomer(customer);
+        await prisma.customer.update({ where: { id: customer.id }, data });
+    }
 
-  return NextResponse.json({
-    message: `${customers.length} clientes anonimizados`,
-  });
+    return NextResponse.json({
+        message: `${customers.length} clientes anonimizados`,
+    });
 }
